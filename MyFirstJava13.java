bf7251edd443c36f8b4f2da94d7913da158b21cc
filbertnathaniel7112 <filbertnathanielfc21@gmail.com>
2026-08-27 16:23:05 +0700

    Add files via upload

---
+++ MyFirstJava13.java
@@ -0,0 +1,10 @@
+public class MyFirstJava13 {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hello Filbert Nathaniel");
+        System.out.println("Belajar Java itu mudah");
+        System.out.println("Semakin sering mencoba, semakin jago")
+        ;
+    }
+}
---
+++ Tugas01_14.java
@@ -0,0 +1,15 @@
+public class Tugas01_14 {
+
+     public static void main(String[] args) {
+        //Percobaan
+        System.out.println("Ambil bola di nampan A, taruh di nampan C");
+        System.out.println("Ambil bola di nampan B, taruh di nampan A");
+        System.out.println("Ambil bola di nampan C, taruh di nampan B");
+        //Hasil Percobaan
+        System.out.println("Setelah Langkah 1: Nampan A = kosong, Nampan B = bulan, Nampan C = bintang");
+        System.out.println("Setelah Langkah 2: Nampan A = bulan, Nampan B = kosong, Nampan C = bintang");
+        System.out.println("Setelah Langkah 3: Nampan A = bulan, Nampan B = bintang, Nampan C = kosong");
+        //Kesimpulan
+        System.out.println("Kesimpulan: Pernyataan yang benar adalah (a) dan (e)");
+    }
+}
